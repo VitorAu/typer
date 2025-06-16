c425d315c65ced7e40f982cb587705835df71424
VitorAu <vitoraugustosantiago@outlook.com.br>
2025-06-16 13:16:28 -0300

    fix: :bug: fix public route component

--- typer_client/src/components/publicRoute.tsx
+++ typer_client/src/components/publicRoute.tsx
@@ -3,6 +3,6 @@ import { JSX, useContext } from "react";
 import { Navigate } from "react-router";
 
 export function PublicRoute({ children }: { children: JSX.Element }) {
-  const { isAuth } = useContext(UserContext);
-  return isAuth ? <Navigate to="/" /> : children;
+  const { isAuth, user } = useContext(UserContext);
+  return isAuth ? <Navigate to={`/menu/${user?.id}`} /> : children;
 }
